7298c020ecc2ffd181fc605e5cf08835d000b93f
Tristan Bull <tristan.tbull@gmail.com>
2020-05-11 12:22:42 -0700

    adding versium search project

---
+++ precache-manifest.58ea57f1cf361e18aa057fba9e0e9f0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7616020d5c013cdb5bd04958697efd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a7365ac9f2192d62b61",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "3d6bfc1b9cb57b812de8",
+    "url": "/static/css/main.8133e899.chunk.css"
+  },
+  {
+    "revision": "0a7365ac9f2192d62b61",
+    "url": "/static/js/2.bcdfce34.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/static/js/2.bcdfce34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d6bfc1b9cb57b812de8",
+    "url": "/static/js/main.76193ae8.chunk.js"
+  },
+  {
+    "revision": "9b7282a2f949ad7e54e2",
+    "url": "/static/js/runtime-main.04ef06f9.js"
+  }
+]);
